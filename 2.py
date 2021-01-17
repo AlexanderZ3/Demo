70b5e59a8d7d5d7ee40c57fbbe224e78f4a7380c
Alex Zhang <zezhong001@e.ntu.edu.sg>
2021-01-18 02:49:44 +0800

    to test push function of second branch of local repository

--- 2.py
+++ 2.py
@@ -6,3 +6,6 @@ c = b
 
 sdfs = 2324
 "I made the second change to 2.py and add a sdfs variable."
+
+
+# i made this change to test if the change in deve branch will influence the remote github repository
